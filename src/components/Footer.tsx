fa9b1467ed730cc0d9b599d3e17d3514e68d224f
Gilson96 <grafael99@gmail.com>
2025-01-20 01:06:53 +0000

    Added linkedIn and github links in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router";
 
 const Footer = () => {
     return (
-        <footer className="h-[3rem] w-full flex justify-between items-center p-[2%] bg-black text-white small-screen:h-[5rem]">
+        <footer className="h-[4rem] w-full flex justify-between items-center p-[2%] bg-black text-white small-screen:h-[5rem]">
             <p className='h-full flex items-center border-r-2 pr-2 border-neutral-400'>
                 <h2
                     className='hidden text-xs small-screen:block small-screen:font-bold small-screen:text-2xl'>RT-Movies</h2>
@@ -12,8 +12,8 @@ const Footer = () => {
             </p>
             <p className="text-xs px-[2%] italic small-screen:text-base">Created by Gilson de Almeida - 2024</p>
             <div className="flex">
-               <Link className="hover:text-neutral-500" to={'https://linkedin.com/in/gilson-de-almeida-a51ba1194/'}><TiSocialLinkedinCircular className="h-5 w-5 small-screen:h-[3rem] small-screen:w-[3rem]" /></Link>
-              <Link className="hover:text-neutral-500" to={'https://github.com/Gilson96/RT-movies'}><TiSocialGithubCircular className="h-5 w-5 small-screen:h-[3rem] small-screen:w-[3rem]" /></Link>
+               <Link className="hover:text-neutral-500" to={'https://linkedin.com/in/gilson-de-almeida-a51ba1194/'}><TiSocialLinkedinCircular className="h-7 w-7 small-screen:h-[3rem] small-screen:w-[3rem]" /></Link>
+              <Link className="hover:text-neutral-500" to={'https://github.com/Gilson96/RT-movies'}><TiSocialGithubCircular className="h-7 w-7 small-screen:h-[3rem] small-screen:w-[3rem]" /></Link>
             </div>        
         </footer>
     )
