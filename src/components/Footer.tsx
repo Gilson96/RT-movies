c5d77cdc931792e6698c8ec98c0cdd869e95d569
Gilson96 <grafael99@gmail.com>
2025-01-20 00:51:52 +0000

    Added linkedIn and github links in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,13 +7,13 @@ const Footer = () => {
         <footer className="h-full w-full flex justify-between items-center p-[2%] bg-black text-white">
             <p className='h-full flex items-center border-r-2 pr-2 border-neutral-400'>
                 <h2
-                    className='hidden small-screen:block small-screen:font-bold small-screen:text-xl small-screen:text-2xl'>RT-Movies</h2>
-                <FilmIcon className='h-9 w-9' />
+                    className='hidden text-xs small-screen:block small-screen:font-bold small-screen:text-2xl'>RT-Movies</h2>
+                <FilmIcon className='h-5 w-5 small-screen:h-10 small-screen:w-10' />
             </p>
-            <p className="px-[2%] italic">Created by Gilson de Almeida - 2024</p>
+            <p className="text-xs px-[2%] italic small-screen:text-base">Created by Gilson de Almeida - 2024</p>
             <div className="flex">
-               <Link className="hover:text-neutral-500" to={'https://linkedin.com/in/gilson-de-almeida-a51ba1194/'}><TiSocialLinkedinCircular className="h-[3rem] w-[3rem]" /></Link>
-              <Link className="hover:text-neutral-500" to={'https://github.com/Gilson96/RT-movies'}><TiSocialGithubCircular className="h-[3rem] w-[3rem]" /></Link>
+               <Link className="hover:text-neutral-500" to={'https://linkedin.com/in/gilson-de-almeida-a51ba1194/'}><TiSocialLinkedinCircular className="h-5 w-5 small-screen:h-[3rem] small-screen:w-[3rem]" /></Link>
+              <Link className="hover:text-neutral-500" to={'https://github.com/Gilson96/RT-movies'}><TiSocialGithubCircular className="h-5 w-5 small-screen:h-[3rem] small-screen:w-[3rem]" /></Link>
             </div>        
         </footer>
     )
